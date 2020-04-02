79fc1c7a680c285527623e385aece06ce8ff4182
otakugt <otakugt@yahoo.es>
2020-04-02 09:59:34 -0600

    Agregando el Mantenimiento de Alumnos

--- Proyectos Netbeans/JavaApplication2/src/Hola1.java
+++ Proyectos Netbeans/JavaApplication2/src/Hola1.java
@@ -13,8 +13,10 @@ public class Hola1 extends javax.swing.JFrame {
     /**
      * Creates new form Hola1
      */
+    private MantenimientoAlumnos2 nuevaVentana2;
     public Hola1() {
         initComponents();
+        
     }
 
     /**
@@ -32,6 +34,7 @@ public class Hola1 extends javax.swing.JFrame {
         jMenuItem1 = new javax.swing.JMenuItem();
         jMenuItem2 = new javax.swing.JMenuItem();
         jMenu2 = new javax.swing.JMenu();
+        jMenuItem3 = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -49,6 +52,11 @@ public class Hola1 extends javax.swing.JFrame {
         jMenu1.setText("Catalogos");
 
         jMenuItem1.setText("Mantenimiento Alumnos");
+        jMenuItem1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem1ActionPerformed(evt);
+            }
+        });
         jMenu1.add(jMenuItem1);
 
         jMenuItem2.setText("Mantenimiento Maestros");
@@ -57,6 +65,15 @@ public class Hola1 extends javax.swing.JFrame {
         jMenuBar1.add(jMenu1);
 
         jMenu2.setText("Salir");
+
+        jMenuItem3.setText("Salir del sistema");
+        jMenuItem3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem3ActionPerformed(evt);
+            }
+        });
+        jMenu2.add(jMenuItem3);
+
         jMenuBar1.add(jMenu2);
 
         setJMenuBar(jMenuBar1);
@@ -79,6 +96,18 @@ public class Hola1 extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jMenuItem3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem3ActionPerformed
+        // TODO add your handling code here:
+        dispose();
+    }//GEN-LAST:event_jMenuItem3ActionPerformed
+
+    private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
+        // TODO add your handling code here:
+        nuevaVentana2 = new MantenimientoAlumnos2();
+        jDesktopPane1.add (nuevaVentana2);
+        
+    }//GEN-LAST:event_jMenuItem1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -122,5 +151,6 @@ public class Hola1 extends javax.swing.JFrame {
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JMenuItem jMenuItem1;
     private javax.swing.JMenuItem jMenuItem2;
+    private javax.swing.JMenuItem jMenuItem3;
     // End of variables declaration//GEN-END:variables
 }
